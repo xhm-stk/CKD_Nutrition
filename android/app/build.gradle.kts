41cd08c99a2449ed70ff6d7524d737a034396be6
xhm-stk <stknpsg@gmail.com>
2026-07-22 17:56:45 +0700

    ci: revert compileSdk to stable 35 to resolve Android 16 preview build errors

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,14 +10,14 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
-    compileSdk = 36
+    compileSdk = 35
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.ckd_nutrition_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
-        targetSdk = 36
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
